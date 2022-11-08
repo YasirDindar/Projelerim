64b8fa01d25878f88a87b682dde04aa2cf3b495b
yasirdindar <myasirdindar@gmail.com>
2022-11-08 15:42:55 +0300

    day05

---
+++ src/day05/Ternary_Soru3.java
@@ -0,0 +1,20 @@
+package day05;
+
+import java.util.Scanner;
+
+public class Ternary_Soru3 {
+    /*
+     //Soru 2- Kullanicidan bir ucgenin 3 kenar uzunlugunu alin, ucgen eskenar ise
+        //“Eskenar ucgen” yazdirin, degilse “Eskenar degil” yazdirin.
+    */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Ucgenin 3 kenar uzunlugunu giriniz");
+        int kenar1 = scan.nextInt();
+        int kenar2 = scan.nextInt();
+        int kenar3 = scan.nextInt();
+
+        System.out.println(kenar1==kenar2 && kenar2==kenar3 ? "Eskenar ucgen" : "Eskenar ucgen degil");
+    }
+
+}
