e153b5b7b967b0944e0eeee155f8475c52821021
yasirdindar <myasirdindar@gmail.com>
2022-11-12 10:33:49 +0300

    day09

---
+++ src/day09/ForLoopsSoru.java
@@ -0,0 +1,29 @@
+package day09;
+
+import java.util.Scanner;
+
+public class ForLoopsSoru {
+    /*
+    Soru-Kullanicidan pozitif bir sayi alin, 1’den baslayarak tum tamsayilari yazdirin, sira
+    -3 ile bolunebilen bir sayiya gelirse, sayi yerine fizz
+    -5 ile bolunebilen bir sayiya gelirse sayi yerine buzz
+    -hem 3 hem 5 ile bolunebilen bir sayiya gelirse sayi yerine fizzBuzz yazdirin
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Pozitif sayi giriniz : ");
+        int sayi = scan.nextInt();
+
+        for (int i = 0; i <=sayi ; i++) {
+            if (i%3==0 && i%5==0){
+                System.out.print("FizzBuzz\n");
+            } else if (i%3==0) {
+                System.out.print("Fizz ");
+            } else if (i%5==0) {
+                System.out.print("Buzz ");
+            }else {
+                System.out.print(i+ " ");
+            }
+        }
+    }
+}
---
+++ src/day09/NestedForLoopCarpimTablosuOlusturma.java
@@ -0,0 +1,13 @@
+package day09;
+
+public class NestedForLoopCarpimTablosuOlusturma {
+    //1-10 arasindaki sayilar icin carpim tablosu olusturalim
+    public static void main(String[] args) {
+        for (int i = 1; i <=10 ; i++) {
+            for (int j = 1; j <=10 ; j++) {
+                System.out.print(i*j + " ");
+            }
+            System.out.println("");
+        }
+    }
+}
