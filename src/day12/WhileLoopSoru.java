85526ee3123fa3352ece2f92240b1df475914253
yasirdindar <myasirdindar@gmail.com>
2022-11-15 10:33:09 +0300

    day12

---
+++ src/day12/WhileLoopSoru.java
@@ -0,0 +1,29 @@
+package day12;
+
+import java.util.Scanner;
+
+public class WhileLoopSoru {
+    /*
+    Kullanicidan toplanmak uzere tamsayilar alin.
+    Kuullanici 0 a basarsa sayi alma islemini bitirin.
+
+    Kullancinin kac sayi girdigini ve bu sayilarin toplaminin kac oldugunu yazdirin
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int sayi = 1;
+        int sayac = 0;
+        int toplam =0;
+
+        while (sayi !=0){
+            System.out.println("Lutfen toplamak icin bir tamsayi giriniz.Bitirmek icin 0' basiniz");
+            sayi=scan.nextInt();
+
+            if (sayi!=0){
+                sayac++;
+                toplam += sayi;
+            }
+        }
+        System.out.println("Girilen "+sayac +" sayinin toplami : "+toplam);
+    }
+}
---
+++ src/day12/WhileLoopSoru2.java
@@ -0,0 +1,28 @@
+package day12;
+
+import java.util.Scanner;
+
+public class WhileLoopSoru2 {
+    public static void main(String[] args) {
+         /*
+        kullanicidan toplanmak uzere sayi alin.
+        sayilarin toplami 500 e esit olur veya gecerse
+
+        girilen sayi adedi,
+        girilen sayilarin toplami ve
+        "Bu kadar yeter"yazdirin.
+          */
+        Scanner scan = new Scanner(System.in);
+        double sayi = 0;
+        double toplam = 0;
+        int sayac =0;
+
+        while (toplam<500){
+            System.out.println("Toplamak icin sayi giriniz");
+            sayi = scan.nextDouble();
+            toplam +=sayi;
+            sayac++;
+        }
+        System.out.println("Girilen "+sayac + " sayinin toplami : " + toplam+ " oldu.Bu kadar yeter");
+    }
+}
