4c6304a1560398b937ef3c05e1bb4254d8a30b34
yasirdindar <myasirdindar@gmail.com>
2022-11-17 14:01:25 +0300

    day14

---
+++ src/day14/ArmstrongSayi.java
@@ -0,0 +1,32 @@
+package day14;
+
+public class ArmstrongSayi {
+    /*
+    Check if the integer is an Armstrong numbers
+    Armstrong sayi:herhangi bir sayinin rakamlarinin kupleri toplami o sayiyi veriyorsa sayi  Armstrong sayidir
+
+               153 = 1*1*1 + 5*5*5 + 3*3*3 = 153
+               370 = 3*3*3 + 7*7*7 + 0*0*0 = 370
+     */
+    public static void main(String[] args) {
+        int sayi = 153 ;
+
+        int yuzlerBasamagi = sayi%10;
+        sayi = sayi/10;
+        int onlarBasamagi = sayi%10;
+        sayi = sayi/10;
+        int birlerBasamagi = sayi%10;
+
+        sayi = (sayi*100) + (onlarBasamagi*10) + (yuzlerBasamagi);
+
+        int kontrol = (yuzlerBasamagi*yuzlerBasamagi*yuzlerBasamagi)+
+                      (onlarBasamagi*onlarBasamagi*onlarBasamagi)+
+                      (birlerBasamagi*birlerBasamagi*birlerBasamagi);
+
+        if (sayi==kontrol){
+            System.out.println("Amstrong sayi");
+        }else {
+            System.out.println("Amstrong sayi degil");
+        }
+    }
+}
---
+++ src/day14/StringIfadeyiTersCevirme.java
@@ -0,0 +1,21 @@
+package day14;
+
+import java.util.Scanner;
+
+public class StringIfadeyiTersCevirme {
+    /*
+    Kullanicidan bir kelime isteyin ve girilen kelimeyi tersten yazdirin
+     */
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Lutfen bir kelime giriniz : ");
+        String kelime = scan.nextLine();
+        String kelimeninTersi = "";
+
+        for (int i = kelime.length()-1 ; i >=0; i--) {
+            kelimeninTersi += kelime.charAt(i);
+        }
+        System.out.println(kelimeninTersi);
+    }
+
+}
